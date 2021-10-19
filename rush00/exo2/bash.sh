c2e64faff5d3a978bac10ca04bb074ca997f8b6c
matvki <m.vecchi@it-students.fr>
2021-10-19 13:50:51 +0200

    update exo2 rush

--- rush00/exo2/bash.sh
+++ rush00/exo2/bash.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-grep 'Cameron\|Preston' tcpd_department.csv >> tcpd_officers.txt
-
-grep '8' tcpd_officers.txt >> tcpd_expert_officer.txt
+awk -F ',' '$2 == "Officer" && $3 > 6' tcpd_department.csv
 
